e36b8659f6276d791c4d487316dcf0782b91a40f
DarkLite1 <BrechtGijbels@gmail.com>
2021-10-13 15:16:25 +0200

    removed unneeded code

--- Toolbox.PermissionMatrix.psm1
+++ Toolbox.PermissionMatrix.psm1
@@ -4,41 +4,35 @@ Function Get-AdUserPrincipalNameHC {
         [String[]]$Name
     )
 
-    try {
-        $notFound = @()
+    $notFound = @()
 
-        $result = foreach ($N in  ($Name | Sort-Object -Unique)) {
-            $adObject = Get-ADObject -Filter "ProxyAddresses -eq 'smtp:$N' -or SAMAccountName -eq '$N'" -Property 'mail'
+    $result = foreach ($N in  ($Name | Sort-Object -Unique)) {
+        $adObject = Get-ADObject -Filter "ProxyAddresses -eq 'smtp:$N' -or SAMAccountName -eq '$N'" -Property 'mail'
 
-            if ($adObject.Count -ge 2) {
-                throw "Multiple results found for name '$N': $($adObject.Name)"
-            }
-    
-            if (-not $adObject) {
-                $notFound += $N
-                Continue
-            }
-    
-            $adUsers = if ($adObject.ObjectClass -eq 'group') {
-                Get-ADGroupMember $adObject -Recursive
-            }
-            elseif ($adObject.ObjectClass -eq 'user') {
-                $adObject
-            }
-    
-            $adUsers | Get-ADUser |
-            Where-Object { $_.Enabled } |
-            Select-Object -ExpandProperty 'UserPrincipalName'
+        if ($adObject.Count -ge 2) {
+            throw "Multiple results found for name '$N': $($adObject.Name)"
         }
     
-        @{
-            notFound          = $notFound
-            userPrincipalName = $result | Sort-Object -Unique
-        }    
+        if (-not $adObject) {
+            $notFound += $N
+            Continue
+        }
+    
+        $adUsers = if ($adObject.ObjectClass -eq 'group') {
+            Get-ADGroupMember $adObject -Recursive
+        }
+        elseif ($adObject.ObjectClass -eq 'user') {
+            $adObject
+        }
+    
+        $adUsers | Get-ADUser | Where-Object { $_.Enabled } |
+        Select-Object -ExpandProperty 'UserPrincipalName'
     }
-    catch {
-        throw "Failed converting email address or SamAccountName to userPrincipalName: $_"       
-    }
+    
+    @{
+        notFound          = $notFound
+        userPrincipalName = $result | Sort-Object -Unique
+    }    
 }
 
 Export-ModuleMember -Function * -Alias *
